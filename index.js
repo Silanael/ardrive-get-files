cecfcb16400879fe3228fbcf956f9edb9ed45a1c
Silanael <sila@silanael.com>
2021-10-08 19:18:05 +0300

    WIP: Project builds and runs.

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 //
-// ardrive_get_files.tx
+// ardrive_get_files.ts
 // Silanael 2021-10-08_01
 //
-//import * as ardrive from 'ardrive-core-js';
-//import { arDriveCommunityOracle } from 'ardrive-core-js';
+import * as ardrive from 'ardrive-core-js';
+import { arDriveCommunityOracle } from 'ardrive-core-js';
 
 function main ()
 {
