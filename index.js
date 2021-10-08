a5e1e0fdbd06d8c8f1340e91a62296bc866d4b73
Silanael <sila@silanael.com>
2021-10-08 20:59:22 +0300

    WIP: Prior to changing approach.

--- index.js
+++ index.js
@@ -4,11 +4,56 @@
 // Silanael 2021-10-08_01
 //
 import * as ardrive from 'ardrive-core-js';
-import { arDriveCommunityOracle } from 'ardrive-core-js';
+import { arDriveCommunityOracle, arweave } from 'ardrive-core-js';
 
-function main ()
+
+// Why does this have to be async?
+async function main ()
 {
-    console.log ("Testing")
+    console.log ("Fetching data..");
+
+    const txes = await ardrive.getAllMyDataFileTxs ("zPZe0p1Or5Kc0d7YhpT5kBC-JUPcDzUPJeMz2FdFiy4", "a44482fd-592e-45fa-a08a-e526c31b87f1", 0);
+
+
+    console.log ("Filename,DataTXID,MetadataTXID");
+        
+    let user = 
+    {
+        login: 'foo',
+        dataProtectionKey: '0',
+        walletPrivateKey: '0',
+        walletPublicKey: 'zPZe0p1Or5Kc0d7YhpT5kBC-JUPcDzUPJeMz2FdFiy4',
+        syncFolderPath: '/tmp',
+        autoSyncApproval: 0
+    };
+    
+    await ardrive.setupDatabase ("./.tmp");
+    let arduser = await ardrive.addNewUser ("foo", user);
+    
+    
+    /*
+    const len = txes.length;
+    for (let c = 0; c < 1; c++)
+    {
+        let entity_type = txes[c].node.tags.find ( ({name}) => name == "Entity-Type").value;
+        
+        if (entity_type == "file")
+        {
+            let metadata_tx_id = txes[c].node.id;
+            let file_id = txes[c].node.tags.find ( ({name}) => name == "File-Id").value;            
+            let data_tx_id = "cafebabe"
+            
+            //let file = await ardrive.getPublicFileData (metadata_tx_id);
+            //let file = await ardrive.getPublicFileEntity (txes[c], file_id);            
+            let file = await ardrive.getFileMetaDataFromTx (txes[c], arduser);
+            
+
+            console.log ("File:" + file);
+            //console.log (filename + "," + data_tx_id);
+        }
+   
+    }
+    */
 }
 
 main ()
